7fc8dc636466d403bdac7fbed3830f711e3fbd71
TaylorMater <rt.taylor.riley@gmail.com>
2024-01-13 01:59:39 -0600

    exploring dotnet 6.0+ project creation in vscode

---
+++ Course_1/Section_3/PlaygroundThree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace PlaygroundThree;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
